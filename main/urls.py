f2814262b37d168c90775e4c7d0b191c66c0e0f5
Malik <tmalik8@dxc.com>
2020-11-09 23:27:20 +0530

    Initial

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from main import views
+
+urlpatterns = [
+    path('article', views.create_article, name='create_article'),
+    path('author/<int:pk>', views.author, name='get_author'),
+    path('article/<int:pk>', views.article, name='get_article'),
+    path('', views.index, name='index')
+]
